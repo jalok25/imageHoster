e0e0290e580931a5bf838ca269cd4bce4cab8606
Alok Ranjan <jalok25@gmail.com>
2020-10-25 01:22:35 +0530

    Password Check Feature Added

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,9 +40,19 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+        if(userService.passwordCriteriaSatisfied(user.getPassword())) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }
+        else {
+            String errorMessage = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            UserProfile profile = new UserProfile();
+            user.setProfile(profile);
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError", errorMessage);
+            return "users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
--- src/main/java/ImageHoster/service/UserService.java
+++ src/main/java/ImageHoster/service/UserService.java
@@ -32,4 +32,38 @@ public class UserService {
         }
     }
 
+    //This function is called at the time of user registration
+    //It'll check the minimum password criteria that has to be satisfied
+    //In order to successfully register a User.
+    //It'll take the password as the input and do necessary check and if the condition satisfies
+    //it'll return true otherwise false.
+    public boolean passwordCriteriaSatisfied(String password) {
+        if(password.length() < 3) {
+            return  false;
+        }
+        boolean hasChar = false;
+        boolean hasInt = false;
+        boolean hasSpecialChar = false;
+        String specialCharacters = "!@#$%^&*()-+',./:;<=>?[]_`{|}";
+        int i = 0;
+        while (i < password.length())
+        {
+            if (Character.isDigit(password.charAt(i)))
+            {
+                hasInt = true;
+            }
+            else if (Character.isAlphabetic(password.charAt(i))) {
+                hasChar = true;
+            }
+            else if (specialCharacters.contains(Character.toString(password.charAt(i)))) {
+                hasSpecialChar = true;
+            }
+            if (hasChar && hasInt && hasSpecialChar)
+            {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
 }
